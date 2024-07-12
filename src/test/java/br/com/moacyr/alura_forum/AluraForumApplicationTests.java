1e544c88cb8430cfe9769f5a4131bf064de762ea
moacyr <soares.moacyr.r@gmail>
2024-07-12 11:54:05 -0300

    Starting the project

---
+++ src/test/java/br/com/moacyr/alura_forum/AluraForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moacyr.alura_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
